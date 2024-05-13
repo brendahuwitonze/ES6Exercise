181aad5b7bd1b72b06b68a7b20176ff502848ec6
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-05-13 21:36:58 +0200

    greetUser function

---
+++ greetUser.js
@@ -0,0 +1,8 @@
+// Template Literal Tidbits: Your company website needs a dynamic welcome 
+// message. Write a function greetUser(name) that uses template literals to create a
+//  personalized greeting message like "Welcome back,
+//   ${name}!".
+
+
+let greetUser=(name)=>`Welcome back, ${name}!`
+console.log(greetUser('Brendah'))
